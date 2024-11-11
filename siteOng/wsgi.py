202c2c62bf64c36ade0d6311a1505ca5f0fd9507
Le0z1nk <leoamorim132@gamil.com>
2024-11-11 18:08:09 -0300

    corrigindo deploy 2

--- siteOng/wsgi.py
+++ siteOng/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'siteOng.settings')
 
 application = get_wsgi_application()
+
+WSGI_APPLICATION = 'appOng.wsgi.app'
